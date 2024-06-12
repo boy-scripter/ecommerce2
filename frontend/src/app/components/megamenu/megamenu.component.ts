7f9c75ff149f6579152ae61a15f621d4ecbc0172
shivam <shivam.new2019@gmail.com>
2024-06-12 23:05:11 +0530

    megamebu

---
+++ frontend/src/app/components/megamenu/megamenu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'MegaMenu',
+    templateUrl: 'megamenu.component.html',
+    styleUrls: ['megamenu.component.scss'],
+    standalone: true,
+    imports: []
+})
+
+export class MegamenuComponent {
+
+    
+}
